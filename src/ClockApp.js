7973f4f226b4d7d83fd6d89e24abc5258d285ffe
Samuel Ramirez <samuel_ramirez@live.com.mx>
2022-02-07 22:26:18 -0600

    funcionalidad de pantalla extendida

--- src/ClockApp.js
+++ src/ClockApp.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ClockScreen } from './components/ClockScreen';
 import { Settings } from './components/Settings';
 import { Toolbar } from './components/Toolbar';
 
 
 export const ClockApp = () => {
+
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false);
+  
+
   return (
     <>
-      <Toolbar />
+      <Toolbar 
+        isExpanded={ isExpanded }
+        handleExpanded={ setIsExpanded }
+        isSettingsOpen={ isSettingsOpen }
+        handleSettings={ setIsSettingsOpen }
+      />
+
       <ClockScreen />
-      <Settings />
+
+      <Settings/>
     </>
   )
 }
--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
-export const Toolbar = () => {
+export const Toolbar = ({ isExpanded, handleExpanded, isSettingsOpen, handleSettings }) => {
+
+  const fullSize = () => {
+    handleExpanded( !isExpanded );
+
+    if ( !isExpanded ) {
+      document.documentElement.requestFullscreen();
+    } else {
+      document.exitFullscreen();
+    }
+  }
+
   return (
     <menu className='toolbar-pannel'>
-      Toolbar
+      <ul className='toolbar-menu'>
+        <li onClick={ () => handleSettings( !isSettingsOpen ) }>
+          { isSettingsOpen ? 'Close settings' : 'Open settings' }
+        </li>
+
+        <li onClick={ fullSize }> 
+          { isExpanded ? 'Close' : 'Expand' }
+        </li>
+      </ul>
     </menu>
   );
 };
