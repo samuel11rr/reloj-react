4f2b12d6274dfb002fe2c2b075d015fcd4fc6ba1
Samuel Ramirez <samuel_ramirez@live.com.mx>
2022-04-12 18:06:26 -0500

    service worker agregado

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ClockApp } from './ClockApp';
 import './styles/styles.scss';
+import { register as registerServiceWorker } from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <ClockApp />,
   document.getElementById('root')
 );
+
+registerServiceWorker();
